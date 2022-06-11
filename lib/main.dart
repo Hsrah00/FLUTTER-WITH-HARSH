a726d77d6bf457f03aab46ce004a2f9e2b899c4d
HARSH <harshpanddey9324@gmail.com>
2022-06-12 02:07:37 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    
+  
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text("wlc to 30 days of flutter"),
-        ),
-      ),
-    ); // MaterialApp
+      home: HomePage(),
+    ); 
   }
 }
